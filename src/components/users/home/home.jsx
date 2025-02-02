866aeb1510981f81ea0ee38a17de32639ebd9014
alipnf <alipnf@proton.me>
2025-02-02 22:37:47 +0700

    feat(home): add testimoni section component

--- src/components/users/home/home.jsx
+++ src/components/users/home/home.jsx
@@ -1,11 +1,15 @@
 import CategoryList from "./category-list";
 import HeroSection from "./hero-section";
+import OrderPlatforms from "./order-platfrom";
+import Testimoni from "./testimoni";
 
 const Home = () => {
   return (
     <div className="pt-4 md:pt-6 lg:pt-10">
       <HeroSection />
       <CategoryList />
+      <Testimoni />
+      <OrderPlatforms />
     </div>
   );
 };
