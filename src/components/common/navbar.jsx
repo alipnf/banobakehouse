505d06f4d2e1db39a11db96e24a7a17f5c81911d
alipnf <alipnf@proton.me>
2025-02-02 21:47:38 +0700

    refactor(navbar): increase navbar max-width

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from "lucide-react";
 const Navbar = () => {
   return (
     <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
-      <nav className="mt-4 relative max-w-5xl md:backdrop-blur-xl bg-opacity-30 w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900 dark:border-neutral-700">
+      <nav className="mt-4 relative max-w-6xl md:backdrop-blur-xl bg-opacity-30 w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900 dark:border-neutral-700">
         <div className="px-4 md:px-0 flex justify-between items-center">
           <div className="flex items-center">
             <NavLink
