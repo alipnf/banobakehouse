dce8a6a152fa3794696b9293f7f509a00b1cf46c
alipnf <alipnf@proton.me>
2025-02-09 22:56:09 +0700

    feat(auth): add login and register pages

---
+++ src/pages/quest/register-page.jsx
@@ -0,0 +1,11 @@
+import Register from "../../components/auth/register";
+
+const RegisterPage = () => {
+  return (
+    <div className="pb-5">
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
