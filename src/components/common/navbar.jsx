8fa051003e58e6c2bc0bba3fda0217e54647a25d
alipnf <alipnf@proton.me>
2025-02-11 20:59:58 +0700

    refactor(navbar): extract nav item as component and update styles

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -3,6 +3,21 @@ import ToggleTheme from "./toggle-theme";
 import useAuthStore from "../../store/use-auth-store";
 import { handleLogout } from "../../services/firebase/auth-services";
 
+const NavItem = ({ to, children }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) =>
+      `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 ${
+        isActive
+          ? "border-gray-800 font-medium text-dark dark:border-light dark:text-light"
+          : "border-transparent text-secondary hover:text-dark dark:text-neutral-400 dark:hover:text-light"
+      } focus:outline-none`
+    }
+  >
+    {children}
+  </NavLink>
+);
+
 const Navbar = () => {
   const { user } = useAuthStore();
 
@@ -10,34 +25,19 @@ const Navbar = () => {
     <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
       <nav className="mt-4 relative max-w-6xl backdrop-blur-xl bg-opacity-30 w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900 dark:border-neutral-700">
         <div className="px-4 md:px-0 flex justify-between items-center">
-          <div className="flex items-center">
-            <NavLink
-              to="/"
-              className="flex-none rounded-md text-xl inline-block font-semibold focus:outline-none focus:opacity-80 dark:text-primary"
-              aria-label="Brand"
-            >
-              Banobakehouse
-            </NavLink>
-          </div>
-
-          <div className="md:hidden flex justify-center items-center gap-3">
+          <NavLink to="/" className="text-xl font-semibold dark:text-primary">
+            Banobakehouse
+          </NavLink>
+          <div className="md:hidden flex gap-3">
             <ToggleTheme />
-
             <button
               type="button"
-              className="hs-collapse-toggle flex justify-center items-center size-10 border border-gray-200 text-gray-600 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-              id="hs-navbar-header-floating-collapse"
-              aria-expanded="false"
-              aria-controls="hs-navbar-header-floating"
-              aria-label="Toggle navigation"
+              className="hs-collapse-toggle size-10 border rounded-full flex items-center justify-center dark:border-neutral-700 dark:text-neutral-400 hover:bg-gray-200 dark:hover:bg-neutral-700"
               data-hs-collapse="#hs-navbar-header-floating"
             >
               <svg
-                className="hs-collapse-open:hidden shrink-0 size-3.5"
+                className="hs-collapse-open:hidden size-3.5"
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="2"
@@ -49,11 +49,8 @@ const Navbar = () => {
                 <line x1="3" x2="21" y1="18" y2="18" />
               </svg>
               <svg
-                className="hs-collapse-open:block hidden shrink-0 size-4"
+                className="hs-collapse-open:block hidden size-4"
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="2"
@@ -69,52 +66,18 @@ const Navbar = () => {
 
         <div
           id="hs-navbar-header-floating"
-          className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
-          aria-labelledby="hs-navbar-header-floating-collapse"
+          className="hidden hs-collapse md:block transition-all duration-300"
         >
-          <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 ${
-                  isActive
-                    ? "border-gray-800 font-medium text-gray-800 dark:border-neutral-200 dark:text-neutral-200"
-                    : "border-transparent text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
-                } focus:outline-none`
-              }
-              aria-current="page"
-            >
-              Beranda
-            </NavLink>
-            <NavLink
-              to="/product"
-              className={({ isActive }) =>
-                `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 ${
-                  isActive
-                    ? "border-gray-800 font-medium text-gray-800 dark:border-neutral-200 dark:text-neutral-200"
-                    : "border-transparent text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
-                } focus:outline-none`
-              }
-            >
-              Produk
-            </NavLink>
-            <NavLink
-              to="/wishlist"
-              className={({ isActive }) =>
-                `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 ${
-                  isActive
-                    ? "border-gray-800 font-medium text-gray-800 dark:border-neutral-200 dark:text-neutral-200"
-                    : "border-transparent text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
-                } focus:outline-none`
-              }
-            >
-              Daftar Favorit
-            </NavLink>
+          <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
+            {["/", "/product", "/wishlist"].map((path, index) => (
+              <NavItem key={index} to={path}>
+                {["Beranda", "Produk", "Daftar Favorit"][index]}
+              </NavItem>
+            ))}
             <div className="hidden md:flex items-center gap-2">
               <ToggleTheme />
             </div>
-
-            <div className="flex items-center justify-center gap-2">
+            <div className="flex items-center gap-2">
               {user ? (
                 <div className="flex items-center gap-3">
                   <span className="text-gray-700 dark:text-neutral-300">
@@ -128,20 +91,28 @@ const Navbar = () => {
                   </button>
                 </div>
               ) : (
-                <>
+                [
+                  {
+                    to: "/auth/login",
+                    text: "Masuk",
+                    styles:
+                      "border border-gray-300 text-gray-700 hover:bg-gray-100 dark:border-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-700",
+                  },
+                  {
+                    to: "/auth/register",
+                    text: "Daftar",
+                    styles:
+                      "text-white bg-secondary dark:bg-light dark:text-dark",
+                  },
+                ].map(({ to, text, styles }, index) => (
                   <NavLink
-                    to="/auth/login"
-                    className="px-4 py-1.5 text-sm font-medium border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-100 dark:border-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-700"
+                    key={index}
+                    to={to}
+                    className={`px-4 py-1.5 text-sm font-medium rounded-lg ${styles}`}
                   >
-                    Masuk
+                    {text}
                   </NavLink>
-                  <NavLink
-                    to="/auth/register"
-                    className="px-4 py-1.5 text-sm font-medium text-white bg-secondary rounded-lg dark:bg-light dark:text-dark"
-                  >
-                    Daftar
-                  </NavLink>
-                </>
+                ))
               )}
             </div>
           </div>
