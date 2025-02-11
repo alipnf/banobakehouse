5666e0e6a768b0e4c8d96a81893510033df75152
alipnf <alipnf@proton.me>
2025-02-11 20:15:11 +0700

    feat(wishlist): add title to wishlist page

--- src/components/users/wishlist/wishlist.jsx
+++ src/components/users/wishlist/wishlist.jsx
@@ -59,6 +59,10 @@ const Wishlist = () => {
   return (
     <div className="min-h-screen bg-light dark:bg-dark py-6 md:py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-xl sm:text-2xl font-semibold text-center text-secondary dark:text-primary mb-4">
+          Kue yang Disimpan
+        </h2>
+
         {products.length > 0 ? (
           <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-3 lg:gap-6">
             {products.map((product) => (
@@ -66,7 +70,7 @@ const Wishlist = () => {
             ))}
           </div>
         ) : (
-          <p className="col-span-2 lg:col-span-3 text-center text-gray-600 dark:text-gray-300">
+          <p className="col-span-2 lg:col-span-3 text-center text-secondary dark:text-primary">
             Tidak ada produk yang ditemukan.
           </p>
         )}
