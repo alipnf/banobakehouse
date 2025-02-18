311d38ae6b26a134ae35fbea3afe4cd24a3be548
alipnf <alipnf@proton.me>
2025-02-18 19:19:32 +0700

    feat(admin): add products page to admin routes

---
+++ src/pages/admin/index.js
@@ -0,0 +1,4 @@
+export { default as AboutUsPage } from "@/pages/admin/about-us-page";
+export { default as CategoriesPage } from "@/pages/admin/categories-page";
+export { default as FaqPage } from "@/pages/admin/faq-page";
+export { default as ProductsPage } from "@/pages/admin/products-page";
