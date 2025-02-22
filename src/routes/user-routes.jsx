c4b95afdd5507abbbb92059acf1131ad298057a5
alipnf <alipnf@proton.me>
2025-02-22 22:30:15 +0700

    feat(user): protect wishlist route with auth route

--- src/routes/user-routes.jsx
+++ src/routes/user-routes.jsx
@@ -6,6 +6,18 @@ import {
   ProductDetailPage,
   WishlistPage,
 } from "../pages/user";
+import { Navigate } from "react-router-dom";
+import useAuthStore from "@/store/use-auth-store";
+
+const AuthRoute = ({ children }) => {
+  const { user } = useAuthStore();
+
+  if (!user) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
+  return children;
+};
 
 const UserRoutes = () => {
   return (
@@ -13,7 +25,14 @@ const UserRoutes = () => {
       <Route index element={<HomePage />} />
       <Route path="product" element={<ProductPage />} />
       <Route path="product/:id" element={<ProductDetailPage />} />
-      <Route path="wishlist" element={<WishlistPage />} />
+      <Route
+        path="wishlist"
+        element={
+          <AuthRoute>
+            <WishlistPage />
+          </AuthRoute>
+        }
+      />
     </Route>
   );
 };
