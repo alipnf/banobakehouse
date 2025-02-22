c6fe22db30f256d30f957d75b5873a5053f5932a
alipnf <alipnf@proton.me>
2025-02-22 23:15:16 +0700

    feat(wishlist): add remove from wishlist functionality

--- src/components/user/wishlist/wishlist.jsx
+++ src/components/user/wishlist/wishlist.jsx
@@ -23,15 +23,19 @@ const Wishlist = () => {
   }, [setWishlist, userId]);
 
   return (
-    <div className="min-h-screen bg-light dark:bg-dark py-6 md:py-8">
+    <div className="min-h-screen bg-light dark:bg-dark py-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-xl sm:text-2xl font-semibold text-center text-secondary dark:text-primary mb-4">
+        <h2 className="text-xl sm:text-2xl font-semibold text-center text-secondary dark:text-primary mb-5">
           Kue yang Disimpan
         </h2>
         {wishlist.length > 0 ? (
           <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-3 lg:gap-6">
             {wishlist.map((product) => (
-              <ProductCard key={product.id} product={product} />
+              <ProductCard
+                key={product.id}
+                product={product}
+                showRemoveButton={true}
+              />
             ))}
           </div>
         ) : (
