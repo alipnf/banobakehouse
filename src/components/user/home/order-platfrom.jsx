1afc9535ff51537026eed60905350a62b348f180
alipnf <alipnf@proton.me>
2025-02-22 22:12:04 +0700

    refactor(home): move order platform logic to hook

--- src/components/user/home/order-platfrom.jsx
+++ src/components/user/home/order-platfrom.jsx
@@ -1,54 +1,7 @@
-import { useEffect, useState } from "react";
-import { getWebInfo } from "@/services/supabase/about-service";
-import { toast } from "react-toastify";
+import useOrderPlatforms from "@/hooks/user/use-order-platforms";
 
 const OrderPlatforms = () => {
-  const [webInfo, setWebInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  // Fetch data dari Firebase
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const data = await getWebInfo();
-        if (data) {
-          setWebInfo(data);
-        }
-      } catch (error) {
-        console.error("Error fetching web info:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  // Fungsi untuk membuka WhatsApp
-  const openWhatsApp = () => {
-    if (webInfo && webInfo.whatsapp) {
-      const message = encodeURIComponent("Aku ingin pesan kue");
-      window.open(
-        `https://wa.me/${webInfo.whatsapp}?text=${message}`,
-        "_blank",
-      );
-    } else {
-      toast.error("Nomor WhatsApp tidak tersedia.", {
-        position: "top-right",
-      });
-    }
-  };
-
-  // Fungsi untuk membuka Grab
-  const openGrab = () => {
-    if (webInfo && webInfo.grab) {
-      window.open(webInfo.grab, "_blank");
-    } else {
-      toast.error("Link Grab tidak tersedia.", {
-        position: "top-right",
-      });
-    }
-  };
+  const { loading, openWhatsApp, openGrab, webInfo } = useOrderPlatforms();
 
   return (
     <section className="bg-light dark:bg-dark mt-10 pt-4">
